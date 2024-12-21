6137fc3a4ab48d80dfc54f6d7cc3365a92e3d6fe
Nithya ganesh <nithyaganesh4343@gmail.com>
2024-12-21 18:15:57 +0530

    debugging

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -12,13 +12,22 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: 'https://nithyaganesh.netlify.app',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // Include any custom headers
   credentials: true,
 };
-
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));  
+app.use(express.json());  
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] Method: ${req.method}, URL: ${req.originalUrl}, Headers: ${JSON.stringify(req.headers)}, Body: ${JSON.stringify(req.body)}, Query: ${JSON.stringify(req.query)}, IP: ${req.ip}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`[DEBUG] Incoming Headers: ${JSON.stringify(req.headers)}`);
+  next();
+});
 
 const signup = require("./router/signup");
 const connectToDB = require("./config/database");  
