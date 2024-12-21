e51d5dd71196ab5b418852f96bdfdf9b62726282
Nithya ganesh <nithyaganesh4343@gmail.com>
2024-12-21 10:14:36 +0530

    Commit message for main

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -1,22 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 
-
-
 const app = express(); 
-const signup   = require("./router/signup");
+const signup = require("./router/signup");
 const connectToDB = require("./config/database");  
-const {auth }=require("./middlewares/loginAuth"); 
+const { auth } = require("./middlewares/loginAuth"); 
 app.use(express.json())
- 
+
 const cors = require('cors');
 
 signup.use((req, res, next) => {
     console.log('CORS headers:', req.headers);
     next();
-  });
-  
-
+});
 
 const allowedOrigin = /^https:\/\/([a-z0-9-]+\.)?nithyaganesh\.netlify\.app$/;
 const corsOptions = {
@@ -24,11 +20,10 @@ const corsOptions = {
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  };
-   
+};
 
 app.use(cors(corsOptions));
- 
+
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -38,26 +33,23 @@ app.options('*', (req, res) => {
 
 app.use(signup); 
 
-
-
-app.get("/get",(req,res)=>{ 
-    
+app.get("/get", (req, res) => { 
     res.send(`ok`);
 }); 
- 
-app.get("/",auth,(req,res)=>{ 
+
+app.get("/", auth, (req, res) => { 
     res.redirect(`/home`);
 }); 
-connectToDB()
-    .then(()=>{
-     console.log("Connected to MongoDB");
-     const PORT = process.env.PORT || 3000;
 
-     app.listen(PORT,()=>{
-        console.log("server running on http://localhost:3000 successfully");
-    });})
-    .catch((e)=>{
+connectToDB()
+    .then(() => {
+        console.log("Connected to MongoDB");
+        const PORT = process.env.PORT || 3000;
+
+        app.listen(PORT, () => {
+            console.log("server running on http://localhost:3000 successfully");
+        });
+    })
+    .catch((e) => {
         console.log(e);
     });
-
-
